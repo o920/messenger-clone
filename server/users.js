45f8c1459e404d40c384ce1c8a6b99e1c5e9cdae
o920 <o920_@naver.com>
2021-06-26 18:26:00 +0900

    3

--- server/users.js
+++ server/users.js
@@ -1,27 +1,29 @@
-const users = {};
+const users = [];
 
-const addUser = ({id, name, room}) =>{
-    name = name.trim().toLowerCase() // 띄어쓰기 제거
-    room = room.trim().toLowerCase()
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
 
-    const existingUser = users.find((user) => user, room === room && user, name === name);
-    if(existingUser) {
-        return { error : 'User name is taken'};
-    }
-    const user = { id, name, room };
-    users.push(user);
-    return {user}
+  const existingUser = users.find((user) => user.room === room && user.name === name);
+
+  if(!name || !room) return { error: 'Username and room are required.' };
+  if(existingUser) return { error: 'Username is taken.' };
+
+  const user = { id, name, room };
+
+  users.push(user);
+
+  return { user };
 }
 
 const removeUser = (id) => {
-    const index = users.findIndex((user) => user.id === id);
-    if(index !== -1){
-        return users.splice(index, 1)[0]
-    }
+  const index = users.findIndex((user) => user.id === id);
+
+  if(index !== -1) return users.splice(index, 1)[0];
 }
 
 const getUser = (id) => users.find((user) => user.id === id);
 
-const getUsersInRoom = (room) => users.filter((user)=>user.room === room) ;
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-module.exports = {addUser, removeUser, getUser, getUsersInRoom}
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
