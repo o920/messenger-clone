ca67030967bb8e83799d4bfedc773b2c3ce48731
o920 <o920_@naver.com>
2021-06-26 18:12:44 +0900

    2

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Join from './components/Join';
-import Chat from './components/Chat';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
+
 const App = () => (
     <Router>
         <Route path ="/" exact component = {Join} />
